72533982b4dd09b66b5d73da29fa5c835eae2208
sholikul <sholikulardian@gmail.com>
2024-11-26 20:57:03 +0700

    added handle navigate to addTask

--- src/components/TaskEmpty.tsx
+++ src/components/TaskEmpty.tsx
@@ -1,12 +1,18 @@
 import { BiTaskX } from "react-icons/bi";
+import { Button } from "../items/Button";
+import { useNavigate } from "react-router-dom";
 
 export const TaskEmpty = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/add");
+  };
   return (
     <section className='bg-white w-96 flex flex-col gap-2 items-center py-12 rounded-lg mt-24'>
       <BiTaskX className='text-9xl' />
       <p className='text-2xl font-bold'>Oops! No Task</p>
-      <p>You Need to create some tasks to do</p>
-      <button className='bg-[#40A7A0] text-white w-28 h-8 rounded-full hover:bg-[#29706B] font-semibold'>Add Now</button>
+      <p className='mb-4'>You Need to create some tasks to do</p>
+      <Button title='Add Now' handleClick={handleNavigate} />
     </section>
   );
 };
