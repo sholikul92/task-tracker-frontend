553ddacf9bf8db3384252069c8b5539f0498ffb7
sholikul <sholikulardian@gmail.com>
2024-11-26 16:10:10 +0700

    create component header

---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className='flex justify-between mb-10'>
+      <h1 className='text-4xl font-bold'>Task Tracker</h1>
+      <button className='bg-[#40A7A0] text-white w-28 h-8 rounded-full hover:bg-[#29706B] font-semibold'>Add Task</button>
+    </div>
+  );
+};
