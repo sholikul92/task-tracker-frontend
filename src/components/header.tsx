9564c31e77422ebdebf852d5a16ed8c24d4a68b6
sholikul <sholikulardian@gmail.com>
2024-11-26 20:56:41 +0700

    added handle navigate to addTask

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../items/Button";
+
 export const Header = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/add");
+  };
   return (
     <div className='flex justify-between mb-10'>
       <h1 className='text-4xl font-bold'>Task Tracker</h1>
-      <button className='bg-[#40A7A0] text-white w-28 h-8 rounded-full hover:bg-[#29706B] font-semibold'>Add Task</button>
+      <Button title='Add Task' handleClick={handleNavigate} />
     </div>
   );
 };
