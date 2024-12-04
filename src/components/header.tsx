fefd824813563f4fd2ffd4e21036dbf4ee8af65d
sholikul <sholikulardian@gmail.com>
2024-12-04 22:19:57 +0700

    added interface props

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../items/Button";
 
-export const Header = () => {
+interface Props {
+  path: string;
+  value: string;
+}
+
+export const Header = (props: Props) => {
   const navigate = useNavigate();
   const handleNavigate = () => {
-    navigate("/add");
+    navigate(props.path);
   };
   return (
     <div className='flex justify-between mb-10'>
       <h1 className='text-4xl font-bold'>Task Tracker</h1>
-      <Button title='Add Task' handleClick={handleNavigate} />
+      <Button title={props.value} handleClick={handleNavigate} />
     </div>
   );
 };
