ea91cc2a453ad9c15fd7461bf793b4278164ef17
sholikul <sholikulardian@gmail.com>
2024-12-04 22:19:38 +0700

    define route edittask

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { AddTask } from "./pages/AddTask";
+import { EditTask } from "./pages/EditTask";
 import { Task } from "./pages/Task";
 import { Route, Routes } from "react-router-dom";
 
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Task />} />
         <Route path='/add' element={<AddTask />} />
+        <Route path='/edit/:id' element={<EditTask />} />
       </Routes>
     </>
   );
