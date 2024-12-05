44bf4168991bedf2f343917b68c2b4a7f6d5ff3e
sholikul <sholikulardian@gmail.com>
2024-12-05 19:23:06 +0700

    fix: added type to component header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import { Button } from "../items/Button";
+import React from "react";
 
 interface Props {
   path: string;
   value: string;
 }
 
-export const Header = (props: Props) => {
+export const Header: React.FC<Props> = ({ path, value }) => {
   const navigate = useNavigate();
   const handleNavigate = () => {
-    navigate(props.path);
+    navigate(path);
   };
   return (
     <div className='flex justify-between mb-10'>
       <h1 className='text-4xl font-bold'>Task Tracker</h1>
-      <Button title={props.value} handleClick={handleNavigate} />
+      <Button title={value} handleClick={handleNavigate} />
     </div>
   );
 };
