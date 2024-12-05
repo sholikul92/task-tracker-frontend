30d99cbb613df122485637a7037b2ec64778c783
sholikul <sholikulardian@gmail.com>
2024-12-05 19:27:00 +0700

    fix: handle logic for view task by option

--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TaskEmpty } from "../components/TaskEmpty";
 import { TaskItem } from "../components/TaskItem";
 import { Header } from "../components/header";
@@ -18,6 +18,8 @@ export const Task = () => {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [refetch, setRefecth] = useState<boolean>(false);
+  const [taskByOption, setTaskByOption] = useState<ApiResponse[] | null | undefined>(null);
+  const [option, setOption] = useState<string>("All");
   // const base_url = import.meta.env.VITE_BASE_URL;
 
   const fetchApi = async () => {
@@ -38,7 +40,21 @@ export const Task = () => {
     fetchApi();
   }, [refetch]);
 
-  const TaskList = tasks?.map((task) => {
+  useEffect(() => {
+    if (option === "All") {
+      setTaskByOption(tasks);
+    } else if (option === "Completed") {
+      setTaskByOption(tasks?.filter((task) => task.completed === true));
+    } else if (option === "Not-completed") {
+      setTaskByOption(tasks?.filter((task) => task.completed === false));
+    }
+  }, [option, tasks]);
+
+  const handleOption: React.ReactEventHandler<HTMLSelectElement> = (e) => {
+    setOption(e.currentTarget.value);
+  };
+
+  const TaskList = taskByOption?.map((task) => {
     return <TaskItem task={task} key={task.ID} setRefetch={setRefecth} />;
   });
 
@@ -49,7 +65,14 @@ export const Task = () => {
   return (
     <div className='bg-[#E9EDF2] min-h-screen px-56 pt-10'>
       <Header path='/add' value='Add Task' />
-      <div className='flex flex-col items-center'>{loading ? <MoonLoader /> : tasks?.length == 0 ? <TaskEmpty /> : TaskList}</div>
+      <div className='flex justify-end mb-4'>
+        <select name='view-task' id='view-task' className='bg-white w-80 rounded-lg p-2' onChange={handleOption}>
+          <option value='All'>All</option>
+          <option value='Completed'>Completed</option>
+          <option value='Not-completed'>Not Completed</option>
+        </select>
+      </div>
+      <div className='flex flex-col items-center'>{loading ? <MoonLoader /> : taskByOption?.length == 0 ? <TaskEmpty /> : TaskList}</div>
     </div>
   );
 };
